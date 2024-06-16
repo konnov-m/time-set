a18af569aec136002939630763e6b58a5c5022b6
Konnov <strouk7@yandex.ru>
2024-06-16 12:57:45 +0300

    add swagger to UserController

--- user-service/src/main/java/com/example/time_set/controller/UserController.java
+++ user-service/src/main/java/com/example/time_set/controller/UserController.java
@@ -2,18 +2,31 @@ package com.example.time_set.controller;
 
 import com.example.time_set.dto.UserDto;
 import com.example.time_set.service.UserService;
+import com.example.time_set.util.exception.GlobalExceptionHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user-service/user")
 @RequiredArgsConstructor
+@Tag(name = "User API")
 public class UserController {
 
     private final UserService userService;
 
 
-
+    @Operation(summary = "Get a user by id", description = "Returns a userDto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - User was not found", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class)))
+    })
     @GetMapping("/{id}")
     public UserDto getUser(@PathVariable long id) {
         return userService.getUser(id);
